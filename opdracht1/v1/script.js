307769f7a5b749a639e6905211ef1a7a701f5c1f
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-02-06 11:47:42 +0100

    Added some JS

--- opdracht1/v1/script.js
+++ opdracht1/v1/script.js
@@ -1,5 +1,8 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+
 document.getElementById("button").addEventListener("click", function( event ) {
-    // Laat het huidige aantal kliks in het klikelement zien.
-    event.target.textContent = "Aantal kliks: " + event.detail;
-  }, false);
+
+}
+
+var lijst = document.querySelector('ul');
