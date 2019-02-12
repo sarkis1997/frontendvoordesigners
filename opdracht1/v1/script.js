cbbf13ee8b5f889aeb3e5582196645f11169dfe7
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-02-12 22:32:56 +0100

    Cloning of content works

--- opdracht1/v1/script.js
+++ opdracht1/v1/script.js
@@ -1,8 +1,13 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
+function functionButton() {  
 
-document.getElementById("button").addEventListener("click", function( event ) {
+var lijst = document.querySelector("li.godfathermovie");
 
-}
+var cln = lijst.cloneNode(true);
 
-var lijst = document.querySelector('ul');
+var nieuwelijst = document.getElementById("savedlist").appendChild(cln);
+	}
+
+var el = document.querySelector("button");
+el.addEventListener("click", functionButton);
