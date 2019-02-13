026fb20edcb446025216bd298c1990224b35c885
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-02-13 10:40:49 +0100

    Looping of movie list works

--- opdracht1/v1/script.js
+++ opdracht1/v1/script.js
@@ -1,13 +1,17 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
 function functionButton() {  
-
-var lijst = buttonclick.parentElement;
+	
+var lijst = this.parentElement;
 var cln = lijst.cloneNode(true);
 var nieuwelijst = document.getElementById("savedlist").appendChild(cln);
 
 }
 
-var buttonclick = document.querySelector("button");
+var buttonclick = document.querySelectorAll("button");
+// var buttonclick = document.getElementsByTagName("button");
 
-buttonclick.addEventListener("click", functionButton);
+for (i= 0; i < buttonclick.length; i++) {
+	
+	buttonclick[i].addEventListener("click", functionButton);
+}
