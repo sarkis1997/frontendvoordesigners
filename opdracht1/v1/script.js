74f05de170154c9579d7259eb8879f5c383954f4
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-02-13 09:52:11 +0100

    Cleaned js from the html doc

--- opdracht1/v1/script.js
+++ opdracht1/v1/script.js
@@ -7,7 +7,8 @@ var lijst = document.querySelector("li");
 var cln = lijst.cloneNode(true);
 
 var nieuwelijst = document.getElementById("savedlist").appendChild(cln);
-	}
+
+}
 
 var buttonclick = document.querySelector("button");
 buttonclick.addEventListener("click", functionButton);
