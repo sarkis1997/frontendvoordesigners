b045f028c1f4785f3c1057e2c100ed25ca532c8d
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-02-13 11:35:57 +0100

    Veranderd de innerHTML van button

--- opdracht1/v1/script.js
+++ opdracht1/v1/script.js
@@ -1,17 +1,20 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
+//selecteert alle buttons
+var buttonAdd = document.querySelectorAll("button");
+
+//loop door alle buttons en voegt events toe
+for (i= 0; i < buttonAdd.length; i++) {
+	
+	buttonAdd[i].addEventListener("click", ButtonAddMovie);
+}
+
+//Functie die een eleemnt (movie) cloned naar de nieuwe lijst
 function ButtonAddMovie() {  
 
 var lijst = this.parentElement;
 var cln = lijst.cloneNode(true);
 var nieuwelijst = document.getElementById("savedlist").appendChild(cln);
+this.innerHTML = "Movie saved";
 
-}
-
-var buttonAdd = document.querySelectorAll("button");
-// var buttonclick = document.getElementsByTagName("button");
-
-for (i= 0; i < buttonAdd.length; i++) {
-	
-	buttonAdd[i].addEventListener("click", ButtonAddMovie);
 }
