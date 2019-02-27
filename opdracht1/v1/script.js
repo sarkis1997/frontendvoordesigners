68a9b857b1f841d8c0a216ac06fe5246d00b5f6e
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-02-27 09:28:05 +0100

    css update

--- opdracht1/v1/script.js
+++ opdracht1/v1/script.js
@@ -19,18 +19,17 @@ var cln = lijst.cloneNode(true);
 //als de button niet gesaved is, save het dan anders console log niks doen. + nog een if function, verander button tekst.
 if (this.innerHTML !== "Movie saved") {
 	var nieuwelijst = document.getElementById("savedlist").appendChild(cln);
-//veranderd de achtergrondkleur van de button
+	//veranderd de achtergrondkleur van de button
 	this.style.backgroundColor = 'green';
-
-//selecteer alle buttons in de savedlist
-	var savedButtonAdd = document.querySelector("#savedlist").querySelectorAll("button");
-//loop door alle buttons en voegt events toe
-	for (i= 0; i < savedButtonAdd.length; i++) {
-	savedButtonAdd[i].addEventListener("click", ButtonRemoveMovie);
-	}
 	//veranderd de tekst van de button
 	this.innerHTML = "Movie saved";
  	
+	//selecteer alle buttons in de savedlist
+	var savedButtonAdd = document.querySelector("#savedlist").querySelectorAll("button");
+	//loop door alle buttons en voegt events toe
+	for (i= 0; i < savedButtonAdd.length; i++) {
+	savedButtonAdd[i].addEventListener("click", ButtonRemoveMovie);
+	}
 } else {
 	console.log("niks doen");
 	}
