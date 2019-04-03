eb6f13a1b153ce1e2990260b382d5802ea2d9794
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-04-03 16:10:53 +0200

    save as progress (ALL WORKING)

--- opdracht3/script.js
+++ opdracht3/script.js
@@ -21,31 +21,62 @@ request.onreadystatechange = function() {
           var title = document.createElement('h2');
           title.appendChild(document.createTextNode(currentMovie.title));
 
-        // Maakt img element aan
+          // Maakt img element aan
+          var imgContainer = document.createElement('div');
+
           var img = document.createElement('img');
         // img is de cover van currentMovie
           img.src = currentMovie.cover;
 
+//-------- HIER WORDEN DE REVIEWS INGELANDEN --------//
           var reviewsOl = document.createElement('ol');
             for (var j = 0; j < currentMovie.reviews.length; j++) {
               var currentScore = currentMovie.reviews[j].score;
 
               var reviewsLi = document.createElement('li');
-              reviewsLi.appendChild(document.createTextNode(currentScore));
-
+              reviewsLi.appendChild(document.createTextNode(currentScore + ", "));
               reviewsOl.appendChild(reviewsLi);
+            }
+//-------- HIER WORDT DE GEMIDDELDE RATING BEPAALD VAN DE FILM --------//
+            var averageScore = 0;
+            for (var k = 0; k < currentMovie.reviews.length; k++) {
+                averageScore += currentMovie.reviews[k].score;
+            }
+            var avg = averageScore / currentMovie.reviews.length;
+            var avgLi = document.createElement('li');
+            avgLi.appendChild(document.createTextNode(avg));
+
+            //Checkt of er al een review is geplaatst door de innerHTML te checken op een getal.
+            if (isNaN(avgLi.innerHTML)) {
+              avgLi.innerHTML = "Er zijn nog geen beoordelingen voor deze film!";
             }
 
 
+            var li = document.createElement('li');
+            // Voegt li item toe door een text NODE met (movieTitle) inhoud aan te maken
+            var reviewsTitle = document.createElement('h3');
+            reviewsTitle.appendChild(document.createTextNode("Reviews"));
+
+            var scoreTitle = document.createElement('h3');
+            scoreTitle.appendChild(document.createTextNode("Deze film is beoordeeld met een"));
+
         //Voegt de h2 toe aan li
           li.appendChild(title);
-        //Voegt img toe aan li
-          li.appendChild(img);
+        //Voegt de 3 toe aan li
+          li.appendChild(scoreTitle);
+        //voegt de Average van reviews to aan li
+          li.appendChild(avgLi)
+        //Voegt imgContainer toe aan li
+          li.appendChild(imgContainer);
+        //voegt de img toe in de container
+          imgContainer.appendChild(img);
         //Voegt de OL van reviews aan de li
+          li.appendChild(reviewsTitle);
           li.appendChild(reviewsOl);
-        //Plaatst de li in ul
+        //Plaatst de algemene li in ul
           ul.appendChild(li);
     }
+
   } else if (this.readyState == 4 && this.status == 200) {
     //hier komt code voor als readystate of status andere code heeft (bijv 404)
     }
@@ -64,39 +95,35 @@ request.onreadystatechange = function() {
   // console.log(currentImage.classList);
 
   //voegt event keydown toe aan het hele document en maakt functie intern aan.
-  document.addEventListener("keyup", function(event) {
-  	var currentImage = document.querySelector('.active');
-  	var prevImage = currentImage.previousElementSibling;
-  	var nextImage = currentImage.nextElementSibling;
-
-      if (event.keyCode == 37) {
-  	        console.log("Left was pressed");
-  			document.querySelector('.active').classList.remove('active');
-
-    			 	// checkt eerst of of de vorige sibbling geen "anything" (kan van alles zijn) is, dan ...
-    		if (prevImage) {
-    			if (prevImage.tagName === "LI"){
-    	       		console.log('LI gevonden');
-    	       		prevImage.classList.add('active');
-    	     	}
-    	     	 	// anders krijgt de laatste li active
-    	    } else {
-    				document.querySelector("ul > li:last-of-type").classList.add('active');
-    	      	}
-  	   }
-
-     else if (event.keyCode == 39) {
-     console.log("Right was pressed");
-     document.querySelector('.active').classList.remove('active');
-
-  	if (nextImage) {
-  		if (nextImage.tagName === "LI"){
-         		console.log('LI gevonden');
-         		nextImage.classList.add('active');
-         		}
-  		} else {
-         		document.querySelector("ul > li:first-of-type").classList.add('active');
-         		}
-  	}
-  });
 };
+document.addEventListener("keyup", function(event) {
+  var currentImage = document.querySelector('.active');
+  var prevImage = currentImage.previousElementSibling;
+  var nextImage = currentImage.nextElementSibling;
+
+    if (event.keyCode == 37) {
+      document.querySelector('.active').classList.remove('active');
+
+          // checkt eerst of of de vorige sibbling geen "anything" (kan van alles zijn) is, dan ...
+      if (prevImage) {
+        if (prevImage.tagName === "LI"){
+              prevImage.classList.add('active');
+          }
+            // anders krijgt de laatste li active
+        } else {
+          document.querySelector("ul > li:last-of-type").classList.add('active');
+            }
+     }
+
+   else if (event.keyCode == 39) {
+   document.querySelector('.active').classList.remove('active');
+
+  if (nextImage) {
+    if (nextImage.tagName === "LI"){
+          nextImage.classList.add('active');
+          }
+    } else {
+          document.querySelector("ul > li:first-of-type").classList.add('active');
+          }
+  }
+});
